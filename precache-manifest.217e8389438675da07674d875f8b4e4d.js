4e77007863b444af3dd20f49a4c1bcbc5e6a8866
Ricardo Ferreira <ricardo199221@gmail.com>
2020-05-20 16:09:04 +0100

    Updates

---
+++ precache-manifest.217e8389438675da07674d875f8b4e4d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc4186ef5b9f3e49c26d744cfbaed541",
+    "url": "/weather-app/index.html"
+  },
+  {
+    "revision": "5e2a34bc7e6d65039de9",
+    "url": "/weather-app/static/css/main.36b1d440.chunk.css"
+  },
+  {
+    "revision": "125d07a089c52b8837de",
+    "url": "/weather-app/static/js/2.382d9366.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/weather-app/static/js/2.382d9366.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5e2a34bc7e6d65039de9",
+    "url": "/weather-app/static/js/main.7d674931.chunk.js"
+  },
+  {
+    "revision": "8ad2e2c3d0b0209b3902",
+    "url": "/weather-app/static/js/runtime-main.4479c0a4.js"
+  },
+  {
+    "revision": "134764d63e33cffd532d10d26d5b67f3",
+    "url": "/weather-app/static/media/blueSky.134764d6.jpg"
+  }
+]);
